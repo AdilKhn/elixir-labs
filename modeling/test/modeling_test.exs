79800a58e9bf93c0b4ec926bbcfd7ddc77a79d68
Adil Khan <adil.khan@outlook.com>
2018-03-01 19:06:59 -0500

    Adds test

--- modeling/test/modeling_test.exs
+++ modeling/test/modeling_test.exs
@@ -5,4 +5,15 @@ defmodule ModelingTest do
   test "greets the world" do
     assert Modeling.hello() == :world
   end
+
+  test "create an object with an attribute" do
+    person = %Modeling.Person{first_name: "Thor"} 
+    assert person.first_name == "Thor"
+  end
+  
+  test "insert the created object and get the id" do
+    person = %Modeling.Person{first_name: "Thor"} 
+    result = Modeling.Repo.insert person
+    assert elem(result, 1).id > 1
+  end
 end
