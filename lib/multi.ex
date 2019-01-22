f69f6c54d0116a7c7303a338418e76c0047d370a
Adil Khan <adil.khan@outlook.com>
2019-01-22 08:16:22 -0500

    Identify primes

--- lib/multi.ex
+++ lib/multi.ex
@@ -3,6 +3,11 @@ defmodule Multi do
     IO.puts(a + b)
   end
 
+  def is_prime(n) do
+    Enum.each(2..n, &check(n, &1))
+  end
+
+
 
   def listen do
     receive do
@@ -13,5 +18,15 @@ defmodule Multi do
     end
 
     listen()
+  end
+
+  defp check(n,x) do
+    IO.puts("n,x #{n}, #{x}")
+    cond do
+      rem(n,x) > 0 ->
+        IO.puts(false)
+      rem(n,x) == 0 -> 
+        IO.puts(true)
+    end
   end
 end
