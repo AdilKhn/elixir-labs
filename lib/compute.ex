8512b7cbd2d247b29a3feaabed6810f16bee659b
Adil Khan <adil.khan@constructconnect.com>
2019-04-23 16:02:41 -0400

    Adds a naive prime number implementation

---
+++ lib/compute.ex
@@ -0,0 +1,35 @@
+defmodule Compute do
+
+  def start do
+    IO.puts "#{__MODULE__} at your service"
+    loop()
+  end
+
+  def loop do 
+    receive do
+      msg -> IO.puts sum(msg)
+    end
+    loop()
+  end
+
+
+  def get_primes(num) do
+    l = []
+    final  = for n <- 1..num do
+     if prime?(n), do: l ++n
+    end
+    Enum.filter(final, & !is_nil(&1))
+  end
+
+  def prime?(num) do
+    list = Enum.to_list(2..(num-1))
+    !Enum.any?(list, fn x -> rem(num, x) == 0 end)
+  end
+
+  defp sum(list) do
+    Enum.sum(list)
+  end
+
+
+
+end
